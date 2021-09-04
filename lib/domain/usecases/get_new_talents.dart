dad990e5041d8b825b3d5c892e5efa70cca70694
Cristian Bedoya <cristianbedoyavargas@gmail.com>
2021-09-04 18:20:58 -0500

    use cases and repositories implemented

---
+++ lib/domain/usecases/get_new_talents.dart
@@ -0,0 +1,14 @@
+import 'package:bff_prueba/core/errors/failure.dart';
+import 'package:bff_prueba/domain/entities/talent.dart';
+import 'package:bff_prueba/domain/repositories/talent_repository.dart';
+import 'package:dartz/dartz.dart';
+
+class GetNewTalents {
+  final TalentRepository _talentRepository;
+
+  GetNewTalents(this._talentRepository);
+
+  Future<Either<Failure, List<Talent>>> call() {
+    return _talentRepository.getNewTalents();
+  }
+}
---
+++ lib/domain/usecases/get_relevant_talents.dart
@@ -0,0 +1,14 @@
+import 'package:bff_prueba/core/errors/failure.dart';
+import 'package:bff_prueba/domain/entities/talent.dart';
+import 'package:bff_prueba/domain/repositories/talent_repository.dart';
+import 'package:dartz/dartz.dart';
+
+class GetRelevantTalents {
+  final TalentRepository _talentRepository;
+
+  GetRelevantTalents(this._talentRepository);
+
+  Future<Either<Failure, List<Talent>>> call() async {
+    return _talentRepository.getRelevantTalents();
+  }
+}

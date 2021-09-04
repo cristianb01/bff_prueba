63d190560aefc29ad630ab629b4272e47e467ceb
Cristian Bedoya <cristianbedoyavargas@gmail.com>
2021-09-04 17:43:00 -0500

    talent repository and entity implemented

---
+++ lib/domain/entities/talent.dart
@@ -0,0 +1,27 @@
+import 'package:equatable/equatable.dart';
+
+class Talent extends Equatable {
+  dynamic id;
+  final String firstName;
+  final String? lastName;
+  final String profession;
+  final int relevance;
+  final int age;
+
+  Talent(
+      {this.id,
+      required this.firstName,
+      this.lastName,
+      required this.relevance,
+      required this.profession,
+      required this.age});
+
+  @override
+  List<Object?> get props => [
+        id,
+        firstName,
+        profession,
+        relevance,
+        age,
+      ];
+}
---
+++ lib/domain/repositories/talent_repository.dart
@@ -0,0 +1,9 @@
+import 'package:bff_prueba/core/errors/failure.dart';
+import 'package:bff_prueba/domain/entities/talent.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class TalentRepository {
+  Future<Either<Failure, List<Talent>>> getRelevantTalents();
+
+  Future<Either<Failure, List<Talent>>> getNewTalents();
+}

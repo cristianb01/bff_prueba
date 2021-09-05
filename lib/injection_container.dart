c049d92d8fe61b9d669cb8c7d2e7d66d34362460
cristianb01 <cristianbedoyavargas@gmail.com>
2021-09-05 16:38:18 -0500

    dependecy injection implemented

---
+++ lib/injection_container.dart
@@ -0,0 +1,17 @@
+import 'package:bff_prueba/data/repositories/talent_repository.dart';
+import 'package:bff_prueba/domain/repositories/talent_repository.dart';
+import 'package:bff_prueba/domain/usecases/get_relevant_talents.dart';
+import 'package:bff_prueba/presentation/cubit/relevant_talents/relevant_talents_cubit.dart';
+import 'package:get_it/get_it.dart';
+
+final sl = GetIt.instance;
+
+Future<void> init() async {
+  sl.registerFactory(() => RelevantTalentsCubit(sl()));
+
+  sl.registerLazySingleton(() => GetRelevantTalents(sl()));
+
+  sl.registerLazySingleton<ITalentRepository>(() => TalentRepository());
+
+  //sl.registerLazySingleton(() => http.Client());
+}
